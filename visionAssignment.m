7486551089a35798880acbfafb909384f7ac9f79
naveay <jyxin@uci.edu>
2013-11-21 23:34:24 -0800

    1

--- visionAssignment.m
+++ visionAssignment.m
@@ -1,7 +1,7 @@
 % This script will run all of the code necessary for the project
 
 clear all;
-aMatrix = setupAMatrix('data_points.txt','colors_for_data.txt','images/initCalibPoint.jpg'); % Read in the 6 recorded points and find A
+aMatrix = setupAMatrix('data_points.txt','colors_for_data.txt','images/calibProbe1.jpg'); % Read in the 6 recorded points and find A
 callibM = findCallibration(aMatrix); % Find the intrinsic matrix
 %normCallibM = callibM/callibM(3,4);
 normCallibM = callibM;
@@ -10,10 +10,10 @@ newM = normCallibM(1:3,1:3);
 [rMatrix,qMatrix] = rq(newM);
 transColumn = inv(rMatrix)*normCallibM(:,4);
 extrinsic = [qMatrix transColumn; 0 0 0 1];
-initProbePoint = [14.25;0;9.125;1];
+initProbePoint = [11;5.25;8.1875;1]; 
 probeInCameraCoords = extrinsic*initProbePoint; 
 
-aMatrixTestPoint = setupAMatrix('data_points.txt','colors_for_data.txt','images/testCalibPoint.jpg');
+aMatrixTestPoint = setupAMatrix('data_points.txt','colors_for_data.txt','images/calibProbe2.jpg');
 callibM = findCallibration(aMatrixTestPoint); % Find the intrinsic matrix
 %normCallibM = callibM/callibM(3,4);
 normCallibM = callibM;
