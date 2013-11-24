a432b7f6599324b5e1c4ea1d120a7323b2963f19
zackattacknyu <zackattacknyu@gmail.com>
2013-11-24 00:52:29 -0800

    changed code for setting up matrix

--- setupAMatrix.m
+++ setupAMatrix.m
@@ -3,10 +3,9 @@
 % The ordering of the points from first to last is
 % Green, Blue, Red, Gold, Purple, Yellow
 
-function aMatrix = setupAMatrix(realWorldVectorTextFile,colorValueTextFile,imageFileName)
+function aMatrix = setupAMatrix(realWorldVectorTextFile,imagePoints)
     fileID = fopen(realWorldVectorTextFile, 'r');
     dataPoints = fscanf(fileID, '%f');
-    imagePoints = getImagePoints(imageFileName,colorValueTextFile,12);
     
     xV = [];
     yV = [];
@@ -15,18 +14,26 @@ function aMatrix = setupAMatrix(realWorldVectorTextFile,colorValueTextFile,image
     vV = [];
     
     %imagePoints = {[81,1165],[2359,744],[2037,1855],[2293,2821],[1090,3198],[1176,1360]};
+
+   for i=1:length(imagePoints)
+       if mod(i, 2) == 0
+          vV = [vV; imagePoints(i)];
+       else
+          uV = [uV; imagePoints(i)];
+       end
+   end
     
-    for i=1:length(imagePoints)
-       temp = imagePoints{i};
-       uV(i) = temp(1);
-       vV(i) = temp(2);
-    end
+    
     for i=1:3:18
         xV = [xV, dataPoints(i)];
         yV = [yV, dataPoints(i+1)];
         zV = [zV, dataPoints(i+2)];
     end
-    
+    xV
+    yV
+    zV
+    uV
+    vV
     A = [];
     for i=1:6
         A = [A; -xV(i) -yV(i) -zV(i) -1 0 0 0 0 uV(i)*xV(i) uV(i)*yV(i) uV(i)*zV(i) uV(i);];
@@ -34,5 +41,6 @@ function aMatrix = setupAMatrix(realWorldVectorTextFile,colorValueTextFile,image
     end
     
     aMatrix = A; % The final A matrix of Ax = 0
+    fclose(fileID);
     
 end
--- visionAssignment.m
+++ visionAssignment.m
@@ -1,7 +1,7 @@
 % This script will run all of the code necessary for the project
 
 clear all;
-% aMatrix = setupAMatrix('data_points.txt','colors_for_data_calibProb.txt','images/calibProbe1.jpg'); % Read in the 6 recorded points and find A
+% aMatrix = setupAMatrix('data_points_block.txt','locationTextFiles/blockProbeIPoints.txt'); % Read in the 6 recorded points and find A
 % callibM = findCallibration(aMatrix); % Find the intrinsic matrix
 % %normCallibM = callibM/callibM(3,4);
 % normCallibM = callibM;
@@ -10,25 +10,49 @@ clear all;
 % [rMatrix,qMatrix] = rq(newM);
 % transColumn = inv(rMatrix)*normCallibM(:,4);
 % extrinsic = [qMatrix transColumn; 0 0 0 1];
-% initProbePoint = [11;5.25;8.1875;1]; 
+% initProbePoint = [11.0625;0;6.875;1]; 
 % probeInCameraCoords = extrinsic*initProbePoint; 
 
-probeInCameraCoords = [5.55139;0.3983109;3.114695;1];
-
+probeInCameraCoords = [-0.3829;6.0913;1.0739;1];
+imagePoints = [];
 realPoints = [];
-images = dir('C:\Users\kevin\Documents\GitHub\prod');
-for i = 4:(length(images)-1)
-    aMatrix = setupAMatrix('data_points.txt','colors_for_data_prodPictures.txt',strcat('C:\Users\kevin\Documents\GitHub\prod\',images(i).name));
-    callibM = findCallibration(aMatrix); % Find the intrinsic matrix
-    %normCallibM = callibM/callibM(3,4);
-    normCallibM = callibM;
-    newM = normCallibM(1:3,1:3);
-    %transColumn = inv(newM)*normCallibM(:,4);
-    [rMatrix,qMatrix] = rq(newM);
-    transColumn = inv(rMatrix)*normCallibM(:,4);
-    extrinsic = [qMatrix transColumn; 0 0 0 1];
-    realPoints = [realPoints; (inv(extrinsic)*probeInCameraCoords)'];
+fileID = fopen('locationTextFiles/IPlanePoints.txt', 'r');
+prodImagePoints = fscanf(fileID, '%f');
+for i = 1:length(prodImagePoints)
+    imagePoints = [imagePoints; prodImagePoints(i)];
+    
+    if(mod(i,12) == 0)
+        aMatrix = setupAMatrix('data_points_redux.txt',imagePoints); % Read in the 6 recorded points and find A
+        callibM = findCallibration(aMatrix); % Find the intrinsic matrix
+        %normCallibM = callibM/callibM(3,4);
+        normCallibM = callibM;
+        newM = normCallibM(1:3,1:3);
+        %transColumn = inv(newM)*normCallibM(:,4);
+        [rMatrix,qMatrix] = rq(newM);
+        transColumn = inv(rMatrix)*normCallibM(:,4);
+        extrinsic = [qMatrix transColumn; 0 0 0 1];
+        %probeInCameraCoords = [5.55139;0.3983109;3.114695;1];
+        realPoints = [realPoints; (inv(extrinsic)*probeInCameraCoords)'];
+        imagePoints = [];
+    end
 end
+
+
+
+% realPoints = [];
+% images = dir('C:\Users\kevin\Documents\GitHub\prod');
+% for i = 4:(length(images)-1)
+%     aMatrix = setupAMatrix('data_points.txt','colors_for_data_prodPictures.txt',strcat('C:\Users\kevin\Documents\GitHub\prod\',images(i).name));
+%     callibM = findCallibration(aMatrix); % Find the intrinsic matrix
+%     %normCallibM = callibM/callibM(3,4);
+%     normCallibM = callibM;
+%     newM = normCallibM(1:3,1:3);
+%     %transColumn = inv(newM)*normCallibM(:,4);
+%     [rMatrix,qMatrix] = rq(newM);
+%     transColumn = inv(rMatrix)*normCallibM(:,4);
+%     extrinsic = [qMatrix transColumn; 0 0 0 1];
+%     realPoints = [realPoints; (inv(extrinsic)*probeInCameraCoords)'];
+% end
 
 % aMatrixTestPoint = setupAMatrix('data_points.txt','colors_for_data_calibPoint.txt','images/calibProbe2.jpg');
 % callibM_TestPoint = findCallibration(aMatrixTestPoint); % Find the intrinsic matrix
