85f8279255dca88a3d78927ba3002b59d16a24d9
zackattacknyu <zackattacknyu@gmail.com>
2013-11-24 23:02:48 -0800

    made final code to submit

---
+++ visionAssignment_book.m
@@ -0,0 +1,41 @@
+% This script will run all of the code necessary for the project
+
+clear all;
+pixelValues = [1946 749 1137 1035 1174 1545 1097 2865 660 1993 367 1410];
+aMatrix = setupAMatrix('data_points_block.txt',pixelValues); % Read in the 6 recorded points and find A
+callibM = findCallibration(aMatrix); % Find the intrinsic matrix
+%normCallibM = callibM/callibM(3,4);
+normCallibM = callibM;
+
+newM = normCallibM(1:3,1:3);
+%transColumn = inv(newM)*normCallibM(:,4);
+[rMatrix,qMatrix] = rq(newM);
+transColumn = inv(rMatrix)*normCallibM(:,4);
+extrinsic = [qMatrix transColumn; 0 0 0 1];
+initProbePoint = [10.0625;0.0625;5.6875;1];
+probeInCameraCoords = extrinsic*initProbePoint; 
+
+imagePoints = [];
+realPoints = [];
+fileID = fopen('locationTextFiles/IPlanePoints_book.txt', 'r');
+prodImagePoints = fscanf(fileID, '%f');
+for i = 1:length(prodImagePoints)
+    imagePoints = [imagePoints; prodImagePoints(i)];
+    
+    if(mod(i,12) == 0)
+        aMatrix = setupAMatrix('data_points_block.txt',imagePoints); % Read in the 6 recorded points and find A
+        callibM = findCallibration(aMatrix); % Find the intrinsic matrix
+        %normCallibM = callibM/callibM(3,4);
+        normCallibM = callibM;
+        newM = normCallibM(1:3,1:3);
+        %transColumn = inv(newM)*normCallibM(:,4);
+        [rMatrix,qMatrix] = rq(newM);
+        transColumn = inv(rMatrix)*normCallibM(:,4);
+        extrinsic = [qMatrix transColumn; 0 0 0 1];
+        %probeInCameraCoords = [5.55139;0.3983109;3.114695;1];
+        realPoints = [realPoints; (inv(extrinsic)*probeInCameraCoords)'];
+        imagePoints = [];
+    end
+end
+
+scatter3(realPoints(:,1), realPoints(:,3), realPoints(:,2),5);
